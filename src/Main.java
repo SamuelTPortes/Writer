2f5d1f043a017d286254075fa947e84a33b3d731
Samuel <samueltportes@gmail.com>
2025-06-06 17:29:03 -0300

    replace CreateFrame class with Main class and add window closing event handling

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Main {
     public static void main(String[] args) {
-        String[] letters = new String[26];
-        letters[0] ="AAAAA\nA   A\nAAAAA\nA   A\nA   A ";
-        System.out.println(letters[0]);
+        Frame frame = new Frame();
+        Label label = new Label("Arthur viado");
+        label.setAlignment(Label.CENTER);
+
+        frame.add(label);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
