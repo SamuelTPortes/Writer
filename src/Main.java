57df65f3b13b7e5ea34ad6a0d1c342dedb5a602e
Samuel <samueltportes@gmail.com>
2025-06-06 17:29:03 -0300

    add CreateFrame class and update project to JDK 21

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        String[] letters = new String[26];
-        letters[0] ="AAAAA\nA   A\nAAAAA\nA   A\nA   A ";
-        System.out.println(letters[0]);
-    }
-}
