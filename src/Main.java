3b4b47f164b2b049c3b786e024d0173383b609a3
Samuel <samueltportes@gmail.com>
2025-06-09 08:04:37 -0300

    add button with MouseListener to Main class handling click and hover events

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -6,16 +8,40 @@ public class Main {
     public static void main(String[] args) {
         Frame frame = new Frame();
         Label label = new Label("Arthur viado");
+        Button button = new Button("Ele é mesmo?");
+        frame.setLayout(new FlowLayout());
+
         label.setAlignment(Label.CENTER);
 
+        frame.add(button);
         frame.add(label);
         frame.setSize(500,500);
-        frame.setVisible(true);
+
+        button.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Clicou no Frame na posição: X=" + e.getX() + ", Y=" + e.getY());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button.setBounds((int) (Math.random() * frame.getX()),(int)(Math.random()* frame.getY()), 100,100);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
+
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
+
+        frame.setVisible(true);
     }
 }
